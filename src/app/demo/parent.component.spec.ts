eff441b1e651140fa4618f9acb3abd556ce18ac0
Michal Kurcius <mkurcius@gmail.com>
2018-11-28 23:43:30 +0100

    demo

---
+++ src/app/demo/parent.component.spec.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({selector: 'cmp-child', template: ''})
+class ChildStubComponent {}
+
+describe('ParentComponent', () => {
+  //tests
+});
---
+++ src/app/demo/parent.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'cmp-parent',
+  template: '<cmp-child></cmp-child>',
+})
+export class ParentComponent {}
